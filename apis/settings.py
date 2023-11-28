8fc9aa78183dd515a131e67784fe9f6453fb9cd7
Andrii0302 <144677052+Andrii0302@users.noreply.github.com>
2023-11-28 16:44:23 +0200

    Update settings.py

--- apis/settings.py
+++ apis/settings.py
@@ -1,7 +1,7 @@
 
 import os
 from pathlib import Path
-# import dj_database_url
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -92,8 +92,8 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-# database_url=os.environ.get("DATABASE_URL")
-# DATABASES['default']=dj_database_url.parse(database_url)
+database_url=os.environ.get("DATABASE_URL")
+DATABASES['default']=dj_database_url.parse(database_url)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
